85851c47a6c2e404e9522da3fe4e1f9546736153
81CuongVn <89056222+81CuongVn@users.noreply.github.com>
2023-02-20 21:24:54 +0700

    Created route and people editing view

--- src/views/index.ts
+++ src/views/index.ts
@@ -2,3 +2,4 @@ export { default as LoginPage } from './login/login-page.vue'
 export { default as AdminPage } from './admin/admin-page.vue'
 export { default as RegisterPeoplePage } from './admin/register-people-page.vue'
 export { default as EditPeoplePage } from './admin/edit-people-page.vue'
+
